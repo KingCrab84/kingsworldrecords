dc9305dd85998c0ae38258f982affd5626dc9230
KingCrab84 <121580105+KingCrab84@users.noreply.github.com>
2023-02-17 00:48:50 +0000

    java

--- game.js
+++ game.js
@@ -1,1 +1,40 @@
 const question = document.getElementById("question");
+const choices = Array.from(document.getElementsByClassName("choice-text"));
+
+let persons = {
+    1:{
+        name: "bob",
+        age: 12,
+        gender: "male"
+    },
+    2:{
+        name: "ob",
+        age: 12,
+        gender: "male"
+    },
+    3:{
+        name: "bb",
+        age: 12,
+        gender: "male"
+    },
+    
+};
+
+console.log(Array.from(persons));
+
+let currentQuestion = {};
+let acceptingAnswers = true;
+let score = 0;
+let questionCounter = 0;
+let availableQuestions = [];
+
+let questions = [
+    {
+        question: "how does the chinese flag looks like?",
+        choice1: "yellow and blue",
+        choice1: ""
+        //choice1:
+        //choice1:
+
+    }
+]
