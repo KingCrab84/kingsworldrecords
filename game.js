0cccd7084778bdc09c7520b99f1fca13531e5565
Cuong Lien <Kunglien1984@hotmail.com>
2023-02-20 15:34:44 +0000

    js

--- game.js
+++ game.js
@@ -11,33 +11,33 @@ let questions = [
     {
         question: "What color does the chinese flag have?",
         choice1: "yellow and blue",
-        choice1: "blue"
-        choice3: "red and yellow"
-        choice4: "green"
+        choice1: "blue",
+        choice3: "red and yellow",
+        choice4: "green",
         answer:  3
     },
     {
         question: "What color does the chinese flag have?",
         choice1: "yellow and blue",
-        choice1: "blue"
-        choice3: "red and yellow"
-        choice4: "green"
+        choice1: "blue",
+        choice3: "red and yellow",
+        choice4: "green",
         answer:  3
     },
     {
         question: "What color does the chinese flag have?",
         choice1: "yellow and blue",
-        choice1: "blue"
-        choice3: "red and yellow"
-        choice4: "green"
+        choice1: "blue",
+        choice3: "red and yellow",
+        choice4: "green",
         answer:  3
     },
     {
         question: "What color does the chinese flag have?",
         choice1: "yellow and blue",
-        choice1: "blue"
-        choice3: "red and yellow"
-        choice4: "green"
+        choice1: "blue",
+        choice3: "red and yellow",
+        choice4: "green",
         answer:  3
     }
 ]
@@ -53,6 +53,10 @@ startgame = () => {
 };
 
 getNewQuestion = () => {
+    if (availableQuestions.length 0 questionCounter MAX_QUESTIONS) { //tags???
+        return window.location.assign("/end.html");
+    }
+    
     questionCounter++;
     const questionIndex = Math.floor(Math.random() * availableQuestions/length);
     currentQuestion = availableQuestions[questionIndex];
