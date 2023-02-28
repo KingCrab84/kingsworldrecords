77d834ad707906ef0feeb80eafc24a353c7a376c
Cuong Lien <Kunglien1984@hotmail.com>
2023-02-28 22:17:07 +0000

    end

--- end.js
+++ end.js
@@ -2,8 +2,12 @@ const username = document.getElementById("username");
 const saveScoreBtn = document.getElementById("saveScoreBtn");
 const finalScore = document.getElementById("finalScore");
 const mostRecentScore = localStorage.getItem("mostRecentScore");
-finalScore.innerHTML = mostRecentScore; //not workin! video7, 11.00
 
+const highScores = JSON.parse(localStorage.getItem("highScores")) || [];
+
+const MAX_HIGH_SCORES = 5;
+
+finalScore.innerHTML = mostRecentScore; //not workin! video7, 11.00
 
 username.addEventListener('keyup', () => {
     saveScoreBtn.disabled = !username.value; //not workin! video7, 10.00
