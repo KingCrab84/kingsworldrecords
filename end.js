da9995c11ed1596b1c6291a73ef8cceb5593913c
Cuong Lien <Kunglien1984@hotmail.com>
2023-02-28 22:21:52 +0000

    end highscores

--- end.js
+++ end.js
@@ -15,4 +15,15 @@ username.addEventListener('keyup', () => {
 
 saveHighScore = e => {
 e.preventDefault();
+
+    const score = {
+        score: Math.floor(Math.random() * 100),
+        name: username.value
+    };
+    highScores.push(score);
+    highScores.sort((a, b) => b.score - a.score);
+    highScores.splice(5);
+
+    localStorage.setItem("highScores", JSON.stringify(highScores));
+    window.location.assign("/");
 };
